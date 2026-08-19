e80ed975cb7254459ff84d3e1bcda76bc7a6b10c
vedant purohit <purohitwishwaraj908@gmail.com>
2026-08-19 16:12:12 +0530

    Untrack api-gateway contents from remote

--- user-service/src/main/java/com/gateway/user_service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gateway.user_service.repository;
-
-import com.gateway.user_service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
